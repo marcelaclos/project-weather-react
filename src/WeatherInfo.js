b3bd9744e371ee41a77a8099e8c7eb8d15a57c86
Marcela Clos <mars@nexiit.com>
2021-08-27 09:10:37 +0200

    add feelslike

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -24,7 +24,7 @@ export default function WeatherInfo(props) {
         </div>
         <div className="col-6">
           <div className="temperature-container d-flex justify-content-end">
-            <WeatherIcon code={props.data.icon} size={52} />
+            <WeatherIcon code={props.data.icon} size={53} />
             <div>
               <span className="temperature">
                 {Math.round(props.data.temperature)}
